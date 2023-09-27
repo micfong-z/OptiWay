4e9a2890480611bd93b7bbb8c440c0ded5691595
Micfong <micfong2@outlook.com>
2023-09-27 23:48:53 +0800

    Add congestion evaluation

--- scripts/path-convertor.py
+++ scripts/path-convertor.py
@@ -15,9 +15,9 @@ for floor_key in source:
             for i in source[floor_key][side_key]:
                 print(i["nodes"][0], i["nodes"][1], i["dist"], i["type"])
 
-print("""S2-1 G 1000 2
-S2-3 G 1000 2
-S2-4 G 1000 2
-S2-5 G 1000 2
-S2-6 G 1000 2
-S2-7 G 1000 2""")
+print("""S2-1 G 999999 2
+S2-3 G 999999 2
+S2-4 G 999999 2
+S2-5 G 999999 2
+S2-6 G 999999 2
+S2-7 G 999999 2""")
